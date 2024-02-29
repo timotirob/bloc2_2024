e4a672816a1125302c1a6e6db22260583d8e2750
trobert <timothee.robert@gmail.com>
2024-02-29 14:05:59 +0100

    Après refactoring tests

--- sources/Note.php
+++ sources/Note.php
@@ -6,12 +6,41 @@ class Note {
     private int $coefficient ;
     private \DateTime   $dateNote ;
 
-public function __construct($matiere, $valeurNote, $coefficient) {
+public function __construct($matiere, $valeurNote, $coefficient, $dateNote=NULL){
         $this->matiere = $matiere;
         $this->valeurNote = $valeurNote;
         $this->coefficient = $coefficient;
-        $this->dateNote = new \DateTime();
+        //$this->dateNote = new \DateTime();
+       /* if ($dateNote == NULL) {
+            $this->dateNote = new \DateTime();
+        } else {
+            $this->dateNote = new \DateTime($dateNote);
+        }*/
+    $this->dateNote = (is_null($dateNote)) ? new \DateTime() : new \DateTime($dateNote);
     }
+
+    public function getMatiere(): string
+    {
+        return $this->matiere;
+    }
+
+    public function getValeurNote(): float
+    {
+        return $this->valeurNote;
+    }
+
+    public function getCoefficient(): int
+    {
+        return $this->coefficient;
+    }
+
+    public function getDateNote(): \DateTime
+    {
+        return $this->dateNote;
+    }
+
+
+
 
    public function __toString()
     {
--- tests/NoteTest.php
+++ tests/NoteTest.php
@@ -10,8 +10,19 @@ class NoteTest extends TestCase
 
     public function test__toString()
     {
-        $note = new Note("Maths", 15, 2, "2020-01-01");
-        $this->assertEquals("Matière : Maths Note : 15 Coefficient : 2 Date : 2024-02-08", $note->__toString());
+        $note = new Note("Maths", 15, 2, "2024-02-28");
+        $this->assertEquals("Matière : Maths Note : 15 Coefficient : 2 Date : 2024-02-28", $note->__toString());
+
+        $note = new Note("Maths", 15, 2, "2024-02-29");
+        $this->assertEquals("Matière : Maths Note : 15 Coefficient : 2 Date : 2024-02-29", $note->__toString());
+
+
+        $note = new Note("Maths", 15, 2, "2024-03-01");
+        $this->assertEquals("Matière : Maths Note : 15 Coefficient : 2 Date : 2024-03-01", $note->__toString());
+
+        $note = new Note("Maths", 15, 2);
+        $junk = new \DateTime() ;
+        $this->assertEquals("Matière : Maths Note : 15 Coefficient : 2 Date : ".$junk->format('Y-m-d'), $note->__toString());
 
     }
 }
